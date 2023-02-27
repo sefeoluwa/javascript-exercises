3930ba08f6ab7f1d659268b5b91fc763e456c0cd
sefeoluwa <sefeoluwa01@gmail.com>
2023-02-27 07:31:45 +0100

    calculator challenge solved

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
 const add = function(no1, no2) {
-	console.log(no1 + no2)
-};
-
-const subtract = function() {
 	
+  return(no1 + no2);
 };
 
-const sum = function() {
+const subtract = function(a, b) {
 	
+  return (a - b)
 };
 
-const multiply = function() {
+const sum = function(value1, value2) {
+	  if(value1 && value2){
+          return(value1 + value2)
+      } else if(typeof value1 && value2 == undefined){
+          return 0;
+      } 
+      else{
+          return value1;
+      }
+};
+
+
+const multiply = function(value1, value2) {
+  return value1*value2;
 
 };
 
-const power = function() {
-	
+const power = function(value1, value2) {
+	return value1**value2;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let answer = 1
+	if(n == 0 || n == 1){
+    return answer;
+  } else if(n > 1){
+    for(let i = n; i >= 1; i--){
+      answer = answer * i;
+    }   return answer; 
+  } else{
+      return "please input a positive number";
+    
+  }
 };
 
 // Do not edit below this line
